7374c69e882cb2d30188ed5d1350d4ce35be888f
Ben-R-R <tolkienfan72@gmail.com>
2012-10-22 23:48:26 -0500

    Player now falls from ship when switching to walk mode.

--- Player.js
+++ Player.js
@@ -61,7 +61,7 @@ var Player = {
             this.isWalking = !this.isWalking;
             this.pVecWalk.x = this.fly_pVec.x;
             this.pVecWalk.z = this.fly_pVec.z;
-            this.pVecWalk.y = getTerrainHeight(terrainMesh, this.pVecWalk.x, this.pVecWalk.z);
+            this.pVecWalk.y = this.fly_pVec.y;//getTerrainHeight(terrainMesh, this.pVecWalk.x, this.pVecWalk.z);
         }
 		
 		
